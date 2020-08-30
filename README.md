a28c9e10005ea1a2941565d156455f5862466316
Qu4k <pippo.reds@gmail.com>
2020-08-30 14:54:24 +0200

    fix: resolve hanging

--- README.md
+++ README.md
@@ -4,20 +4,36 @@
 [![CI Status](https://img.shields.io/github/workflow/status/denosaurs/exit/check)](https://github.com/denosaurs/exit/actions)
 [![License](https://img.shields.io/github/license/denosaurs/exit)](https://github.com/denosaurs/exit/blob/master/LICENSE)
 
+Handle `SIGQUIT`, `SIGINT`, and `SIGTERM` in your application.
+
+---
+
+> ⚠️ Signals are still not very well supported in deno. As APIs become more
+
+---
+
+## stable this library will be updated. Currently not supported for windows
+
 ```typescript
-import { onExit } from "https://deno.land/x/exit/mod.ts";
+import { onExit, exit } from "https://deno.land/x/exit/mod.ts";
 
 onExit(() => {
   console.log("Program is exiting, doing some cleanup!");
 });
+
+onExit(async () => {
+  console.log("I will be called after the first one!");
+});
+
+exit(0); // needed as signal hooks need to be disposed
 ```
 
-## other
+## Other
 
-### contribution
+### Contribution
 
 Pull request, issues and feedback are very welcome. Code style is formatted with deno fmt and commit messages are done following Conventional Commits spec.
 
-### licence
+### Licence
 
 Copyright 2020-present, the denosaurs team. All rights reserved. MIT license.
--- mod.ts
+++ mod.ts
@@ -1,31 +1,38 @@
-const sync: (() => void)[] = [];
+type Action = () => void | Promise<void>;
 
-export function onExit(action: () => void): void {
-  sync.push(action);
-  if (sync.length === 1) {
-    (async () => {
-      window.onunload = async () => {
-        for (const op of sync) {
-          op();
+const actions: Action[] = [];
+const ss: Deno.SignalStream[] = [];
+
+async function setupHook() {
+  if (Deno.build.os !== "windows") {
+    ss.push(
+      ...[
+        Deno.signals.quit(),
+        Deno.signals.interrupt(),
+        Deno.signals.terminate(),
+      ]
+    );
+    for (const s of ss) {
+      (async () => {
+        for await (const _ of s) {
+          for (const op of actions) {
+            await op();
+          }
+          Deno.exit(-128);
         }
-      };
-      if (Deno.build.os !== "windows") {
-        const ss = [
-          Deno.signals.quit(),
-          Deno.signals.interrupt(),
-          Deno.signals.terminate(),
-        ];
-        for (const s of ss) {
-          (async () => {
-            for await (const _ of s) {
-              for (const op of sync) {
-                op();
-              }
-              Deno.exit(-128);
-            }
-          })();
-        }
-      }
-    })();
+      })();
+    }
   }
+}
+
+export function onExit(action: Action): void {
+  actions.push(action);
+  if (actions.length === 1) {
+    setupHook();
+  }
+}
+
+export function exit(code?: number): never {
+  for (const s of ss) s.dispose();
+  return Deno.exit(code);
 }
